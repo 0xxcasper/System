3c00ad8c27a8d9f6d3d161efc71cf122310f5f28
SangNX <nxsang063@gmail.com>
2019-11-10 22:25:23 +0700

    Initial Commit

---
+++ System/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System
+//
+//  Created by SangNX on 11/10/19.
+//  Copyright © 2019 nxsang063@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
